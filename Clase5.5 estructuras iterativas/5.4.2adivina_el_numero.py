7c0daa0f0736d1559a1d7504c357e355b0c8895e
monii2408 <154949336+monii2408@users.noreply.github.com>
2025-05-31 14:58:52 -0600

    5.4.2adivina_el_numero.py

--- Clase5.5 estructuras iterativas/5.4.2adivina_el_numero.py
+++ Clase5.5 estructuras iterativas/5.4.2adivina_el_numero.py
@@ -1,7 +1,16 @@
 # Adivina el número.
 # Genera un número aleatorio entre 1 y 100 y pide al usuario que lo adivine.
 # El programa debe seguir pidiendo números hasta que acierte.
+'''
+clase:       5
+Tema:        Estructuras iterativas
+Ejercicio:   1
+Descripción: 5.4.1. ¡Adivina el numero!
 
+Autor:       Monica Alvarenga
+Fecha:       2025-06-1
+Estado:      [Terminado]
+'''
 import random
 
 num = random.randrange(1,100)
